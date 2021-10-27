1b65911a2154b0d8c4f343d3e3107b5eb9b4e4ef
Xavier Mercado <xmercado1576@gmail.com>
2021-10-26 17:59:52 -0700

    Messages component functionality

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { posts, users } from './components/WPAPI'; 
+import Messages from './components/Messages';
 
 export default function App() {
   posts();
   users();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Messages />
   );
 }
 
