d1417a2a2cac15d208032184699c727a48aa1350
Xavier Mercado <xmercado1576@gmail.com>
2021-10-29 15:57:30 -0700

    ImageGallery functionality (no upload/send)

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { posts, users } from './components/WPAPI'; 
+import { posts, users } from './components/WPAPI';
 import Messages from './components/Messages';
+import ImageGallery from './components/ImageGallery';
 
 export default function App() {
-  posts();
-  users();
+  // posts();
+  // users();
   return (
-    <Messages />
+    <ImageGallery />
+    // <Messages />
   );
 }
 
--- components/WPAPI.js
+++ components/WPAPI.js
@@ -7,3 +7,19 @@ export const posts = () => {
 export const users = () => {
     return fetch(`${BASE_URL}/wp/v2/users`).then(response => response.json()).then(data => console.log(data))
 }
+
+export const media = () => {
+    return fetch(`${BASE_URL}/wp/v2/media/`).then(response => response.json()).catch(error => console.log(error))
+}
+
+export const comments = () => {
+    return fetch(`${BASE_URL}/wp/v2/comments`).then(response => response.json()).then(data => console.log(data))
+}
+
+export const categories = () => {
+    return fetch(`${BASE_URL}/wp/v2/categories`).then(response => response.json()).then(data => console.log(data))
+}
+
+export const search = () => {
+    return fetch(`${BASE_URL}/wp/v2/search`).then(response => response.json()).then(data => console.log(data))
+}
