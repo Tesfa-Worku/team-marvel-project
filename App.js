2e30cacd4f590e6f9dd9a7918e0a28f3512136dd
Darla <dbrown@alphaworks.tech>
2021-10-29 20:40:54 -0700

    deleting unuse code

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
 import { posts, users } from './components/WPAPI'; 
+import Header from './components/Header';
 
 export default function App() {
   posts();
   users();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
     </View>
   );
+        
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     flex: 1,
+     flexDirection: 'column',
+     justifyContent: 'space-around',
+     padding: 20,
+     marginTop: "100",
+  },
+   developerSection:{
+     flexDirection: "row",
+     justifyContent: 'center', 
+     padding: 10,
+  },
+    logout: {
+     flexDirection: 'row',
+     justifyContent: 'space-around',
+     padding: 10,
   },
 });
+  
