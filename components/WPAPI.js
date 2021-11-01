c8a87b763de490a655db4ceae54f24515cda9c31
Myles <mazehko@alphaworks.tech>
2021-11-01 15:47:09 -0700

    restoring WPAPI.js code.

--- components/WPAPI.js
+++ components/WPAPI.js
@@ -1,10 +1,13 @@
-// endpoints: posts, users, fetch, media, comments, categories, search, blockTypes, blocks
+const BASE_URL = 'https://jualuc1.dreamhosters.com/wp-json';
 
-export const WP_GET = (endpoint, id) => {
-    if (id == null) {
-        return fetch(`${BASE_URL}/wp/v2/${endpoint}/`).then(response => response.json())
-        .catch(error=>error)
-    }
-    return fetch(`${BASE_URL}/wp/v2/${endpoint}/${id}`).then(response => response.json())
-    .catch(error=>error)
+export const posts = () => {
+    return fetch(`${BASE_URL}/wp/v2/posts`).then(response => response.json()).then(data => console.log(data))
+}
+
+export const users = () => {
+    return fetch(`${BASE_URL}/wp/v2/users`).then(response => response.json()).catch(error => console.log(error))
+}
+
+export const media = () => {
+    return fetch(`${BASE_URL}/wp/v2/media/`).then(response => response.json()).catch(error => console.log(error))
 }
