963b8a2661549b86258d464bd25f106e17b56745
Xavier Mercado <xmercado1576@gmail.com>
2021-11-01 15:28:29 -0700

    Strong password requirement added

---
+++ components/Password.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button
+} from 'react-native';
+
+// display alert modal on Login and SignUp:
+// import { checkPasswordStrength } from './Password/'
+// {alertWeak ? displayWeakPassword() : null}
+export const checkPasswordStrength = (passwordInput) => {
+    const [strongPassword, setStrongPassword] = useState(false);
+    const [alertWeak, setAlertWeak] = useState(false);
+
+    const displayWeakPassword = () => (
+        <View style={styles.alertContainer}>
+            <Text>
+                Password must be 8 to 15 characters containing at least a digit, uppercase, lowercase, and symbol!
+            </Text>
+            <View>
+                <Button
+                    title='OK'
+                    onPress={() => setAlertWeak(!alertWeak)}
+                    />
+            </View>
+        </View>
+    );
+
+    // src: https://www.w3resource.com/javascript/form/password-validation.php
+    const check = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+    if (passwordInput.value.match(check)) {
+        setStrongPassword(true);
+    }
+    else {
+        setAlertWeak(!alertWeak);
+        setStrongPassword(false);
+    }
+}
+
+const styles = StyleSheet.create({
+    alertContainer: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
