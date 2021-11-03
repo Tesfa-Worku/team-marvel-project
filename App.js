de133ae940dccc234ef027a57d66eb7bf1cb6517
Myles <mazehko@alphaworks.tech>
2021-11-03 14:12:50 -0700

    edited app.js to fix errors.

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SearchPage from './components/SearchPage';
-import Footer from './components/Footer';
+import { posts, users } from './components/WPAPI'; 
 import Messages from './components/Messages';
-import ImageGallery from './components/ImageGallery';
-import ResetPassword from './components/ResetPassword';
-import SignUp from './components/SignUp';
-import { NavigationContainer } from '@react-navigation/native';
-import Drawer from './components/navigation/drawer';
-import ProfilePage from './components/ProfilePage';
-import ProfileEdit from './components/ProfileEdit';
 
 export default function App() {
   posts();
   users();
-
-  return (    
-    <View>
-      <ProfilePage/>
-      <ProfileEdit/>
-      <ImageGallery/>
-      <SearchPage/>
-      <Footer/>
-      <Messages/>     
-    </View>
-
-
-  export default function App() {
-  return ( 
-    <NavigationContainer>
-        <Drawer />    
-    </NavigationContainer>   
+  return (
+    <Messages />
   );
 }
-};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
