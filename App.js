0c284508264f8ce3fb95aeed33a58f09416ac257
Myles <mazehko@alphaworks.tech>
2021-11-03 14:28:28 -0700

    imported needed elements to App.js.

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { posts, users } from './components/WPAPI'; 
+import ProfilePage from './components/ProfilePage';
+import ProfileEdit from './components/ProfileEdit';
 import Messages from './components/Messages';
+import Footer from './components/Footer';
+import Header from '.components/Header';
 
 export default function App() {
   posts();
   users();
   return (
-    <Messages />
+    <>
+      <Header / >
+      <ProfilePage />
+      <Footer />
+    </>
   );
 }
 
